27973042361ea3d6dc3fab68950e7c82636045e4
Fill Koks <sergey1065@mail.ru>
2017-10-05 23:05:19 +0300

    Modul pattern was added.

--- ImageGallery/Scripts/image-gallery.js
+++ ImageGallery/Scripts/image-gallery.js
@@ -1,93 +1,104 @@
-﻿var offset = 0;
-var imagePerPage = 4;
-var loader = $("<div />", { class: "loader" });
+﻿$(document).ready(function () {
 
-$(document).ready(function () {
-    if (!$("#prev").parent().hasClass("no-link"))
-        $("#prev").click(prevButtonOnClick);
+    var imageGalleryModule = (function () {
 
-    if (!$("#next").parent().hasClass("no-link"))
-        $("#next").click(nextButtonOnClick);
-});
+        var offset = 0;
+        var imagePerPage = 4;
+        var loader = $("<div />", { class: "loader" });
 
-function getImages() {
-    $.ajax({
-        type: "GET",
-        url: "/Home/Index",
-        data: { offset: offset },
-        dataType: "json",
-        beforeSend: function () {
-            $("#image-gallery").append(loader);
-        },
-        success: function (images) {
-            $("img.gallery-image").each(function (index) {
-                var imageSrc;
-                if (images[index] != undefined) {
-                    imageSrc = "/Images/" + images[index].FileName;
-                } else {
-                    imageSrc = "";
+        var getImages = function () {
+            $.ajax({
+                type: "GET",
+                url: "/Home/Index",
+                data: { offset: offset },
+                dataType: "json",
+                beforeSend: function () {
+                    $("#image-gallery").append(loader);
+                },
+                success: function (images) {
+                    $("img.gallery-image").each(function (index) {
+                        var imageSrc;
+                        if (images[index] != undefined) {
+                            imageSrc = "/Images/" + images[index].FileName;
+                        } else {
+                            imageSrc = "";
+                        }
+                        $(this).prop("src", imageSrc);
+                        $(this).parent().prop("href", imageSrc);
+                    });
+                    $("#image-gallery").find(loader).remove();
                 }
-                $(this).prop("src", imageSrc);
-                $(this).parent().prop("href", imageSrc);
             });
-            $("#image-gallery").find(loader).remove();
-        }
-    });
-}
+        };
 
-function prevButtonOnClick(e) {
-    e.preventDefault();
+        var prevButtonOnClick = function (e) {
+            e.preventDefault();
 
-    offset -= imagePerPage;
-    if (offset < 0)
-        offset = 0;
-    getImages();
+            offset -= imagePerPage;
+            if (offset < 0)
+                offset = 0;
+            getImages();
 
-    updatePrevNextButton();
-}
+            updatePrevNextButton();
+        };
 
-function nextButtonOnClick(e) {
-    e.preventDefault();
+        var nextButtonOnClick = function (e) {
+            e.preventDefault();
 
-    offset += imagePerPage;
-    getImages();
+            offset += imagePerPage;
+            getImages();
 
-    updatePrevNextButton();
-}
+            updatePrevNextButton();
+        };
 
-function updatePrevButton()
-{
-    $("#prev").prop("href", "/?offset=" + (offset - imagePerPage));  //надо ли?
+        var updatePrevButton = function () {
+            $("#prev").prop("href", "/?offset=" + (offset - imagePerPage));  //надо ли?
 
-    if (offset == 0) {
-        $("#prev").parent().addClass("no-link");
-        $("#prev").unbind("click");
-    } else if ($("#prev").parent().hasClass("no-link")) {
-        $("#prev").parent().removeClass("no-link");
-        $("#prev").click(prevButtonOnClick);
-    }
-}
-
-function updateNextButton() {
-    $("#next").prop("href", "/?offset=" + (offset + imagePerPage));  //надо ли?
-
-    $.ajax({
-        type: "GET",
-        url: "/Home/CountOfImages",
-        dataType: "json",
-        success: function (count) {
-            if (offset + imagePerPage >= count) {
-                $("#next").parent().addClass("no-link");
-                $("#next").unbind("click");
-            } else if ($("#next").parent().hasClass("no-link")) {
-                $("#next").parent().removeClass("no-link");
-                $("#next").click(nextButtonOnClick);
+            if (offset == 0) {
+                $("#prev").parent().addClass("no-link");
+                $("#prev").unbind("click");
+            } else if ($("#prev").parent().hasClass("no-link")) {
+                $("#prev").parent().removeClass("no-link");
+                $("#prev").click(prevButtonOnClick);
             }
-        }
-    });
-}
+        };
 
-function updatePrevNextButton() {
-    updatePrevButton();
-    updateNextButton();
-}
+        var updateNextButton = function () {
+            $("#next").prop("href", "/?offset=" + (offset + imagePerPage));  //надо ли?
+
+            $.ajax({
+                type: "GET",
+                url: "/Home/CountOfImages",
+                dataType: "json",
+                success: function (count) {
+                    if (offset + imagePerPage >= count) {
+                        $("#next").parent().addClass("no-link");
+                        $("#next").unbind("click");
+                    } else if ($("#next").parent().hasClass("no-link")) {
+                        $("#next").parent().removeClass("no-link");
+                        $("#next").click(nextButtonOnClick);
+                    }
+                }
+            });
+        };
+
+        var updatePrevNextButton = function () {
+            updatePrevButton();
+            updateNextButton();
+        };
+
+        return {
+
+            initPaginationButtons: function () {
+                if (!$("#prev").parent().hasClass("no-link"))
+                    $("#prev").click(prevButtonOnClick);
+
+                if (!$("#next").parent().hasClass("no-link"))
+                    $("#next").click(nextButtonOnClick);
+            },
+
+        };
+    })();
+
+    imageGalleryModule.initPaginationButtons();
+});
